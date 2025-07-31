1c58474e0adaef5cd408d52346d4b2b4e1a84458
Your Nameenghelalbd <informhelal@yahoo.com>
2025-07-31 10:54:23 +0600

    Java Script array

---
+++ array.js
@@ -0,0 +1,8 @@
+const numbers=[12,52,23,26,2];
+const brothers=['Helal', 'Jalal','Jelal', 'Belal','Bilkis','Mohiuddin'];
+const boolean=[true,false,true,false];
+const mixedarray=[20,22,'jalal',25.5,false]
+console.log(numbers);
+console.log(brothers);
+console.log(boolean);
+console.log(mixedarray);
---
+++ index.js
@@ -0,0 +1,12 @@
+const num=[1,2,34,43,23,13,32,21,11,23,31,45,51,56]
+console.log(num);
+// console.log(num.length);
+console.log(num[0]);
+console.log(num[3]);
+console.log(num[10]);
+console.log(num[5]);
+const forth=num[3];
+console.log(forth);
+//  Set or Update Value by Index
+num[1]='Hello world';
+console.log(num[1]);
