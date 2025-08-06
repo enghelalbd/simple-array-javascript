05e86a85bb573fc35420eb0fd4e1105679dbd029
Your Nameenghelalbd <informhelal@yahoo.com>
2025-08-06 15:41:12 +0600

    first commit

--- include.js
+++ include.js
@@ -1,3 +1,11 @@
 const brothers=['jalal','jelal','Helal','Belal'];
+const num=[8,9,4,8,5,7,5,1]
 console.log(brothers.includes('Helal'));
 console.log(brothers.includes('kalam'));
+// 
+console.log(brothers)
+ brothers.sort();
+ console.log(brothers)
+ console.log(num)
+ num.sort();
+ console.log(num)
---
+++ push-pop.js
@@ -0,0 +1,6 @@
+const num=[2,45,85,65,59,56,75];
+console.log(num);
+num.push(85);
+console.log(num);
+num.push(86,88,35,5);
+console.log(num);
