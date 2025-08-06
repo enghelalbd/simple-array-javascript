d7c5e444460bfa1ba761ed4dd060e87dd9ad4777
Your Nameenghelalbd <informhelal@yahoo.com>
2025-08-06 15:49:55 +0600

    Comments Added

--- array.js
+++ array.js
@@ -1,6 +1,10 @@
+//  Number array
 const numbers=[12,52,23,26,2];
+// String Array
 const brothers=['Helal', 'Jalal','Jelal', 'Belal','Bilkis','Mohiuddin'];
+//  Billean Array
 const boolean=[true,false,true,false];
+//  Mixed Array
 const mixedarray=[20,22,'jalal',25.5,false]
 console.log(numbers);
 console.log(brothers);
